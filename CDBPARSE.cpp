86dc9f7198c37f9be725ccd6cd995ade44f2350a
Andrew Maklingham <38868148+DrewFrogs@users.noreply.github.com>
2022-06-20 10:24:02 -0400

    Added files via upload
    build 1

---
+++ CDBPARSE.cpp
@@ -0,0 +1,175 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    string line;
+    ifstream myfile (argv[1]);
+    if (myfile.is_open())
+    {	
+		int check = 0;
+		string procedureName;
+		ofstream outfile;
+		outfile.open("CDB.txt");
+		outfile << "LABEL;SCOPE;NAME;NTERFACE_COMPILE_STATUS;BODY_COMPILE_STATUS;CLASS;ACCESS;INTERFACE;TYPE;NUMBER SCALE FACTORS;SCALE" << '\n';
+        while ( getline (myfile,line) )
+        {
+			if (check == 1){
+				if (line.find("BODY_COMPILE_STATUS") != std::string::npos){
+					//Fine word and print it
+					string bodyStatus;
+					bodyStatus = line.substr(line.find('"')+1);
+					bodyStatus.pop_back();
+					outfile << ';' << bodyStatus << '\n';
+					check = 0;
+				}
+				else if (line.find("BODY_COMPILE_DATE") == std::string::npos && line.find("BODY_COMPILE_TIME") == std::string::npos){
+					outfile << '\n';
+					check = 0;
+				}
+			}
+            if (line.find("UNIT_NAME") != std::string::npos){
+				//Fine word and print it
+				string unitName;
+				unitName = line.substr(line.find('"')+1);
+				unitName.pop_back();
+				procedureName = unitName;
+			    outfile << "UNIT_NAME;" << " ;" << unitName << ';';
+			}
+			else if (line.find("INTERFACE_COMPILE_STATUS") != std::string::npos){
+				//Fine word and print it
+				string interStatus;
+				interStatus = line.substr(line.find('"')+1);
+				interStatus.pop_back();
+			    outfile << interStatus;
+				check = 1;
+			}
+			else if (line.find(".SCOPE") != std::string::npos){
+				//Fine word and print it
+				string scope;
+				string type;
+				scope = line.substr(line.find('"')+1);
+				scope.pop_back();
+				if(line.find("TYPE.REF_DATA") != std::string::npos){
+					type = line.substr(line.find("REF_DATA"));
+					type = type.substr(0, type.find(".", 0));
+					outfile << type << ';' << scope << ';';
+				}
+				else{
+					outfile << scope << ';';
+				}
+			}
+			else if (line.find(".NAME") != std::string::npos){
+				//Fine word and print it
+				string name;
+				string type;
+				name = line.substr(line.find('"')+1);
+				name.pop_back();
+				if(line.find("NUM_PROCEDURE_PARAMETER") != std::string::npos){
+					type = line.substr(line.find("NUM_PROCEDURE_PARAMETER"));
+					type = type.substr(0, type.find(".", 0));
+					outfile << type << ';' << procedureName << ';' << name << ';';
+				}
+				else if (line.find("GEN_DATA") != std::string::npos){
+					type = line.substr(line.find("GEN_DATA"));
+					type = type.substr(0, type.find(".", 0));
+					outfile << type << ';' << procedureName << ';' << name << ';';
+				}
+				else if (line.find("SUBUNITS") != std::string::npos){
+					type = line.substr(line.find("SUBUNITS"));
+					type = type.substr(0, type.find(".", 0));
+					outfile << type << ';' << procedureName << ';' << name << '\n';
+				}
+				else if (line.find("WITH_PACKAGE") != std::string::npos){
+					type = line.substr(line.find("WITH_PACKAGE"));
+					type = type.substr(0, type.find(".", 0));
+					if(line.find("VISIBILITY") != std::string::npos){
+						outfile << "VISIBILITY: " << name << '\n';
+					}
+					else{
+						outfile << type << ';' << procedureName << ';' << name << ", ";
+					}
+					
+				}
+				else{
+					outfile << name << ';';
+				}
+			}
+			else if (line.find(".TYPE") != std::string::npos){
+				//Fine word and print it
+				string typeName;
+				typeName = line.substr(line.find('"')+1);
+				typeName.pop_back();
+			    outfile << " ;" << typeName;
+				if (typeName == "bool"){
+					outfile << '\n';
+				}
+				else{
+					outfile << ';';
+				}
+			}
+			else if (line.find("REF_SCALE") != std::string::npos){
+				//Fine word and print it
+				string scale;
+				scale = line.substr(line.find('"')+1);
+				scale.pop_back();
+			    outfile << scale << '\n';
+			}
+			else if (line.find(".SCALE") != std::string::npos){
+				//Fine word and print it
+				string scale;
+				scale = line.substr(line.find('"')+1);
+				scale.pop_back();
+			    outfile << " ;" << scale << '\n';
+			}
+			else if (line.find(".CLASS") != std::string::npos){
+				//Fine word and print it
+				string Class;
+				Class = line.substr(line.find('"')+1);
+				Class.pop_back();
+			    outfile << " ; ;" << Class << ';';
+			}
+			else if (line.find(".ACCESS") != std::string::npos){
+				//Fine word and print it
+				string access;
+				access = line.substr(line.find('"')+1);
+				access.pop_back();
+			    outfile << access << ';';
+			}
+			else if (line.find("INTERFACE_CLASS") != std::string::npos){
+				//Fine word and print it
+				string interClass;
+				interClass = line.substr(line.find('"')+1);
+				interClass.pop_back();
+			    outfile << " ;" << interClass << '\n';
+			}
+			else if (line.find(".INTERFACE") != std::string::npos){
+				//Fine word and print it
+				string inter;
+				inter = line.substr(line.find('"')+1);
+				inter.pop_back();
+			    outfile << inter << ';';
+			}
+			else if (line.find("NUM_OF_SCALE_FACTORS") != std::string::npos){
+				//Fine word and print it
+				string scaleNum;
+				scaleNum = line.substr(line.find('"')+1);
+				scaleNum.pop_back();
+			    outfile << " ;" << scaleNum;
+				if (scaleNum == "0"){
+					outfile << '\n';
+				}
+				else{
+					outfile << ';';
+				}
+			}
+        }
+        myfile.close();
+		outfile.close();
+    }
+    else cout << "Unable to open file"; 
+
+    return 0;
+}
